1e5b457624b39923fe8f8e28fb0e4b131a75449f
Shannee Ahirwar <shanneeahirwar@gmail.com>
2023-11-16 18:53:59 +0530

    initial completion

---
+++ app.py
@@ -0,0 +1,48 @@
+
+from flask import Flask, request, jsonify
+import joblib
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) 
+# Load the trained SVM classifier and vectorizer
+svm_classifier = joblib.load('model.pkl')
+v = joblib.load('vectorizer.pkl')
+
+# Define the list of labels
+labels = ['Not Hate','Hate']
+
+def predict_hate(text):
+    # Process the input text
+    texts = [text]
+    text_count = v.transform(texts)
+    
+    # Make predictions using the SVM classifier
+    result = svm_classifier.predict(text_count)
+
+    # print(result[0])
+    
+    # Map the result to the corresponding label
+    prediction = labels[result[0]]
+    
+    return prediction
+
+@app.route('/prediction', methods=['POST'])
+def predict():
+    try:
+        # Get input data from the request
+        data = request.json
+        text = data['text']
+        
+        # Call the function
+        prediction = predict_hate(text)
+
+        # Return the prediction as a JSON response
+        response = {'prediction': prediction}
+        return jsonify(response)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080) 
+
--- templates/script.js
+++ templates/script.js
@@ -9,15 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
       alert("Please add some text");
       return;
     }
-    document.getElementById("emotions-meter").style.display = "none"
     const data = {
       text: text,
     };
     console.log(data);
-    var memeImage = document.getElementById("meme-image");
-    let color = "";
-    let result = "";
-    let bgCol = "";
 
     // Send a POST request to the server
     fetch("http://127.0.0.1:8080/prediction", {
@@ -29,54 +24,8 @@ document.addEventListener("DOMContentLoaded", function () {
     })
       .then((response) => response.json())
       .then((res) => {
-        console.log(res);
-        switch (res.predicted_emotion) {
-          case "neutral":
-            color = "gray";
-            result = "Neutral 😐";
-            break;
-          case "joy":
-            color = "green";
-            result = "Joy 😄";
-            bgCol = "rgb(232, 255, 232)";
-            break;
-          case "sadness":
-            result = "Sadness 😢";
-            color = "rgb(28, 60, 149)";
-            bgCol = "rgb(222, 231, 255)";
-            break;
-          case "fear":
-            result = "Fear 😨";
-            color = "rgb(33, 33, 33)";
-            bgCol = "rgb(33, 33, 33)";
-            break;
-          case "surprise":
-            result = "Surprise 😲";
-            color = "rgb(205, 205, 0)";
-            bgCol = "rgb(225, 225, 165)";
-            break;
-          case "anger":
-            result = "Anger 😡";
-            color = "darkred";
-            bgCol = "rgb(244, 194, 194)"
-            break;
-          case "shame":
-            result = "Shame 😳";
-            color = "rgb(255, 121, 143)";
-            bgCol = "rgb(255, 234, 234)";
-            break;
-          case "disgust":
-            result = "Disgust 🤢";
-            bgCol = "rgb(187, 249, 255)"
-            color = "rgb(0, 223, 186)";
-            break;
-          default:
-            resultText.style.color = "black"; // Default color for other sentiments
-        }
-        memeImage.src = `./images/${res.predicted_emotion}.jpg`;
-        resultText.textContent = result;
-        resultText.style.color = color;
-        document.body.style.backgroundColor = bgCol;
+        console.log(res.prediction);
+        resultText.textContent = res.prediction;
       })
       .catch((error) => {
         console.error("Error:", error);
